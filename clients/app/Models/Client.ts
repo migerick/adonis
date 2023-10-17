99fe9d5f3320bb0c92dd4e0b9b3324a3159ebe22
Miguel Flores <mfloresa15@unc.edu.pe>
2023-10-16 22:48:52 -0500

    feat: validation client over 18 yeard old

--- clients/app/Models/Client.ts
+++ clients/app/Models/Client.ts
@@ -2,6 +2,8 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 import { schema } from '@adonisjs/validator/build/src/Schema'
 import { rules } from '@adonisjs/validator/build/src/Rules'
+import {beforeSave} from "@adonisjs/lucid/build/src/Orm/Decorators";
+import { isBefore, differenceInYears } from 'date-fns'
 
 export default class Client extends BaseModel {
   @column({ isPrimary: true })
@@ -32,5 +34,15 @@ export default class Client extends BaseModel {
       motherSurname: schema.string.optional({ trim: true }, [rules.maxLength(100)]),
       birthdate: schema.date({}, [rules.required()]),
     })
+  }
+
+  @beforeSave()
+  public static async validateAge(client: Client) {
+    const now = new Date()
+    if (isBefore(client.birthdate, now) && differenceInYears(now, client.birthdate) >= 18) {
+      return true
+    } else {
+      throw new Error('the client must be at least 18 years old.')
+    }
   }
 }
