502af8d7d152551cc688d605f83054ddb394a0ff
Miguel Flores <mfloresa15@unc.edu.pe>
2023-10-16 22:59:06 -0500

    feat: add route kpi

---
+++ dataanalysis/app/Controllers/Http/KpisController.ts
@@ -0,0 +1,7 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class KpisController {
+  public async index({ response}) {
+    return response.ok({})
+  }
+}
--- dataanalysis/start/routes.ts
+++ dataanalysis/start/routes.ts
@@ -4,7 +4,7 @@
 |--------------------------------------------------------------------------
 |
 | This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
+| for the majority of projects, however you can define routes in different
 | files and just make sure to import them inside this file. For example
 |
 | Define routes in following two files
@@ -19,7 +19,13 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import KpisController from "App/Controllers/Http/KpisController";
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.get('/kpi', async (ctx) => {
+  return new KpisController().index(ctx)
+})
+
